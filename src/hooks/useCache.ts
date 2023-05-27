ddab994ccba4d95e1b557ac9c2e3864bb2ee4bb3
hwangsungjae <edx2fire@likelion.org>
2023-05-27 15:52:10 +0900

    feat : add time expiration feature to useCache hook

--- src/hooks/useCache.ts
+++ src/hooks/useCache.ts
@@ -1,25 +1,44 @@
+import { EXPIRATION_TIME } from "../constants";
 import { crewActions } from "../store/crew";
 import { CharacterDetail } from "../types/fetch-types";
 import { useAppDispatch } from "../utils/RTKhooks";
 
 export const useCache = () => {
   const dispatch = useAppDispatch();
+  const cacheKey = '/crewList'
+  const cacheObj = {
+    data: [],
+    cachedTime: new Date().getTime()
+  };
   const processCache = async (fnc: () => Promise<CharacterDetail[][]>) => {
     try {
       const cache = await caches.open('crew');
       
-      const response = await cache.match('/crewList');
+      const response = await cache.match(cacheKey);
       if (response) {
         const res = await response.json();
-        dispatch(crewActions.fetchCrew(res));
-      } 
-      else {
-        const res = await fnc();
+        const cachedTime = new Date(res.cachedTime).getTime();
+        const currentTime = new Date().getTime();
+        
+        if (currentTime - cachedTime > EXPIRATION_TIME) {
+          const data = await fnc();
+          const newCache = { ...cacheObj, data };
+
+          await cache.put(cacheKey, new Response(JSON.stringify(newCache), {
+            headers: { 'Content-Type': 'application/json' }
+          }));
+          dispatch(crewActions.fetchCrew(data));
+        } else {
+          dispatch(crewActions.fetchCrew(res.data));
+        }
+      } else {
+        const data = await fnc();
+        const newCache = { ...cacheObj, data };
         // Add the fetched resource to the cache
-        await cache.put('/crewList', new Response(JSON.stringify(res), {
+        await cache.put(cacheKey, new Response(JSON.stringify(newCache), {
           headers: { 'Content-Type': 'application/json' }
         }));
-        dispatch(crewActions.fetchCrew(res));
+        dispatch(crewActions.fetchCrew(data));
       }
       // Use the fetched resource  
     } catch (error) {
